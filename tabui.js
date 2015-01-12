8498fb237bd87aba55aa07a17af4ff7eb60263ed
Ducky (Work PC) <mail@ducky.ws>
2015-01-12 16:29:21 +0000

    MOAR attribution!

---
+++ tabui.js
@@ -0,0 +1,14 @@
+/*
+
+     _____     _     _   _ ___ 
+    |_   _|_ _| |__ | | | |_ _|
+      | |/ _` | '_ \| | | || | 
+      | | (_| | |_) | |_| || | 
+      |_|\__,_|_.__/ \___/|___|
+
+    Version: Git
+     Author: Josh Robertson
+    License: Public Domain
+     Source: https://github.com/electricduck/TabUI
+     
+*/
